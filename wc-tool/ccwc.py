23b087291326f54aae82aa97434a8f1935bf9376
James Goodman <james.goodman@thoughtworks.com>
2023-10-17 11:58:28 +0100

    [JG] added the ability to take standard input and refactored a lot to help with this

--- wc-tool/ccwc.py
+++ wc-tool/ccwc.py
@@ -1,55 +1,29 @@
 import argparse
+import sys
 
 
-def count_bytes_in_file(file_path):
-    try:
-        with open(file_path, "rb") as file:
-            bytes_count = len(file.read())
-
-    except FileNotFoundError:
-        print(f"ccwc -c: {file_path}: No such file or directory")
-        return
-
-    print(f"Bytes count: {bytes_count}; File: {file_path}")
+def count_bytes_in_text(text):
+    bytes_count = len(text.encode())
+    return bytes_count
 
 
-def count_lines_in_file(file_path):
-    try:
-        with open(file_path, "rb") as file:
-            line_count = sum(1 for line in file)
-
-    except FileNotFoundError:
-        print(f"ccwc -c: {file_path}: No such file or directory")
-        return
-
-    print(f"Line count: {line_count}; File: {file_path}")
+def count_lines_in_text(text):
+    line_count = text.count("\n") + 1
+    return line_count
 
 
-def count_words_in_file(file_path):
-    try:
-        with open(file_path, "rb") as file:
-            text = file.read()
-            words = text.split()
-            word_count = len(words)
-
-    except FileNotFoundError:
-        print(f"ccwc -c: {file_path}: No such file or directory")
-        return
-    print(f"Word count: {word_count}; File: {file_path}")
+def count_words_in_text(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
 
 
-def count_characters_in_file(file_path):
-    try:
-        with open(file_path, "rb") as file:
-            text = file.read()
-            characters = len(text.decode())
-    except FileNotFoundError:
-        print(f"ccwc -c: {file_path}: No such file or directory")
-        return
-    print(f"Character count: {characters}; File: {file_path}")
+def count_characters_in_text(text):
+    characters = len(text)
+    return characters
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Count the number of bytes in a file.")
     parser.add_argument(
         "-c", "--count", help="Count bytes", default=False, action="store_true"
@@ -67,19 +41,35 @@ if __name__ == "__main__":
         default=False,
         action="store_true",
     )
-    parser.add_argument("file", help="File to count bytes  in")
+    parser.add_argument("file", help="File to count in", nargs="?")
 
     args = parser.parse_args()
 
-    if args.count:
-        count_bytes_in_file(args.file)
-    elif args.lines:
-        count_lines_in_file(args.file)
-    elif args.words:
-        count_words_in_file(args.file)
-    elif args.characters:
-        count_characters_in_file(args.file)
+    if args.file is None:
+        # No filename provided, read from standard input
+        text = sys.stdin.read()
     else:
-        count_bytes_in_file(args.file)
-        count_lines_in_file(args.file)
-        count_words_in_file(args.file)
+        with open(args.file, "rb") as file:
+            text = file.read().decode("utf-8")
+
+    if args.count:
+        bytes_count = count_bytes_in_text(text)
+        print(f"Bytes count: {bytes_count}; File: {args.file or 'Standard Input'}")
+
+    if args.lines:
+        line_count = count_lines_in_text(text)
+        print(f"Line count: {line_count}; File: {args.file or 'Standard Input'}")
+
+    if args.words:
+        word_count = count_words_in_text(text)
+        print(f"Word count: {word_count}; File: {args.file or 'Standard Input'}")
+
+    if args.characters:
+        character_count = count_characters_in_text(text)
+        print(
+            f"Character count: {character_count}; File: {args.file or 'Standard Input'}"
+        )
+
+
+if __name__ == "__main__":
+    main()
--- wc-tool/test_ccwc.py
+++ wc-tool/test_ccwc.py
@@ -1,44 +1,47 @@
 import unittest
-from unittest.mock import patch
 from io import StringIO
+from unittest.mock import patch
 from ccwc import (
-    count_bytes_in_file,
-    count_lines_in_file,
-    count_words_in_file,
-    count_characters_in_file,
+    count_bytes_in_text,
+    count_lines_in_text,
+    count_words_in_text,
+    count_characters_in_text,
 )
 
 
-class TestFileCountingFunctions(unittest.TestCase):
-    def test_count_bytes_in_file(self):
-        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
-            count_bytes_in_file("test.txt")
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, f"Bytes count: {342184}; File: test.txt")
+class TestCountFunctions(unittest.TestCase):
+    def test_count_bytes_in_text(self):
+        text = "This is a test text."
+        bytes_count = count_bytes_in_text(text)
+        self.assertEqual(bytes_count, len(text.encode()))
 
-    def test_count_lines_in_file(self):
-        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
-            count_lines_in_file("test.txt")
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, f"Line count: {7143}; File: test.txt")
+    def test_count_lines_in_text(self):
+        text = "Line 1\nLine 2\nLine 3"
+        line_count = count_lines_in_text(text)
+        self.assertEqual(line_count, 3)
 
-    def test_count_words_in_file(self):
-        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
-            count_words_in_file("test.txt")
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, f"Word count: {58164}; File: test.txt")
+    def test_count_words_in_text(self):
+        text = "This is a test text."
+        word_count = count_words_in_text(text)
+        self.assertEqual(word_count, 5)
 
-    def test_count_characters_in_file(self):
-        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
-            count_characters_in_file("test.txt")
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, f"Character count: {339286}; File: test.txt")
+    def test_count_characters_in_text(self):
+        text = "This is a test text."
+        character_count = count_characters_in_text(text)
+        self.assertEqual(character_count, len(text))
 
-    def test_no_file_present(self):
-        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
-            count_bytes_in_file("unknown.txt")
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, f"ccwc -c: unknown.txt: No such file or directory")
+
+class TestCommandLineArguments(unittest.TestCase):
+    def test_command_line_args(self):
+        with patch("sys.argv", ["ccwc.py", "-c", "-l", "-w", "test.txt"]):
+            with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+                from ccwc import main
+
+                main()
+                output = mock_stdout.getvalue()
+                self.assertIn("Bytes count:", output)
+                self.assertIn("Line count:", output)
+                self.assertIn("Word count:", output)
 
 
 if __name__ == "__main__":
