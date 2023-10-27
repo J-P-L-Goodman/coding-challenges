9d52b76b7336a82019f8b23d940234035de7c7a8
James Goodman Personal <jpl.goodman1997+pro@gmail.com>
2023-10-27 15:22:55 +0100

    [JG] compression_tool: read in a file and count the number of occurances for each character

--- compression_tool/main.py
+++ compression_tool/main.py
@@ -2,6 +2,32 @@ import argparse
 import sys
 
 
+def number_of_occurances(text):
+    char_counts = {}
+    for char in text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
+
+
 def main():
     parser = argparse.ArgumentParser(description="Count the number of bytes in a file.")
     parser.add_argument("file", help="File to count in", nargs="?")
+
+    args = parser.parse_args()
+
+    if args.file is None:
+        print("Invalid: File not valid")
+    else:
+        with open(args.file, "rb") as file:
+            text = file.read().decode("utf-8")
+
+    char_counts = number_of_occurances(text)
+    print(f"{char_counts.get('X')} occurances of X")
+    print(f"{char_counts.get('t')} occurances of t")
+
+
+if __name__ == "__main__":
+    main()
--- compression_tool/tests/test_main.py
+++ compression_tool/tests/test_main.py
@@ -5,10 +5,24 @@ from unittest.mock import patch
 
 class TestCommandLineArguments(unittest.TestCase):
     def test_command_line_args(self):
-        with patch("sys.argv", ["main.py", "test.txt"]):
+        with patch("sys.argv", ["main.py", "gutenberg.txt"]):
             with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
                 from main import main
 
                 main()
                 output = mock_stdout.getvalue()
-                self.assertIn("Bytes count:", output)
+                self.assertIn("333 occurances of X", output)
+                self.assertIn("223000 occurances of t", output)
+
+
+class TestHelperFunctions(unittest.TestCase):
+    def test_number_of_occurances(self):
+        from main import number_of_occurances
+
+        text = "This is a test text."
+        char_counts = number_of_occurances(text)
+        self.assertEqual(char_counts.get("X"), None)
+        self.assertEqual(char_counts.get("t"), 4)
+        self.assertEqual(char_counts.get("i"), 2)
+        self.assertEqual(char_counts.get("s"), 3)
+        self.assertEqual(char_counts.get("."), 1)
